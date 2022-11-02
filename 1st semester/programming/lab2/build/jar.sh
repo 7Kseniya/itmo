981df660a391db0aad69484fd3a1e3d5d940d61f
Ksenya <114088092+7Kseniya@users.noreply.github.com>
2022-11-02 10:49:01 +0300

    Update jar.sh

--- 1st semester/programming/lab2/build/jar.sh
+++ 1st semester/programming/lab2/build/jar.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 
-javac -cp ".:../lib/Pokemon.jar(1)" -d classes/ ../src/ru/ifmo/se/pokemon/*
+javac -cp ".:../lib/Pokemon.jar(1)" -d classes/ ../src/ru/ifmo/se/*
 jar cvmf MANIFEST.MF lab2.jar -C classes/ .
