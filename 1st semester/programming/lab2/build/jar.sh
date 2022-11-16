d2d4a7731a990be3a9280751e929b3902852c21a
Ksenya <114088092+7Kseniya@users.noreply.github.com>
2022-11-16 03:36:09 +0300

    Update jar.sh

--- 1st semester/programming/lab2/build/jar.sh
+++ 1st semester/programming/lab2/build/jar.sh
@@ -1,10 +1,7 @@
 #!bin/bash
 
-javac -cp "src:lib/Pokemon.jar" -d classes/ ../src/ru/ifmo/se/*
-cp -R lib/ build/
-cat > MANIFEST.mf
-echo "Main-Class: ru.ifmo.se.pokemon.Main
-Class-Path: ../lib/Pokemon.jar
-" > MANIFEST.MF
+javac -cp ".:../lib/Pokemon.jar" -d classes/ ../src/ru/ifmo/se/pokemon/*
 jar cvmf MANIFEST.MF lab2.jar -C classes/ .
+
+#компиляция
 java -jar lab2.jar
