b36bc6d289bd95495de48133fc9e84aa6dd95333
Ksenya <114088092+7Kseniya@users.noreply.github.com>
2022-11-18 08:33:57 +0300

    Update lab1.sh

--- 1st semester/bpa/lab1.sh
+++ 1st semester/bpa/lab1.sh
@@ -6,20 +6,22 @@ cd lab0
 mkdir -p ekans8/{claydol,grumpig,torterra}
 touch armaldo3
 echo "satk=7 sdef=8 spd=5" > armaldo3
-touch bayleef6
-touch pikachu1
+touch bayleef6 pikachu1
 echo -e "Ходы Body \nSlam Counter Cover Defence Curl Double-Edge Dynamicpuch Focus Punch \nHelping Hand Iron Tail Knock Off Magnet Rise Mega Kick Mega Punch Mud-Slap Rollout Seismic Toss Shock Wave Signal Beam Sleep Talk Shore \nSwift Thunderpunch Uproar" > pikachu1
 
 mkdir -p ekans8/{claydol,grumpig,torterra}
+mkdir -p gallade2/scythe
 touch gallade2/{purrloin,shinx}
 echo -e "Способности Scratch Growl Assist \nSad-Attack Fury Swipes Pursuit Torment Fake Out Hone Claws Assurance \nSlash Captivate Night Slash Satch Nasty Plot Sucker \nPunch" > purrloin
 echo "Развитые способности Guts" > shinx
-mkdir scyther
+
 
 mkdir -p nidorino3/{shellder,omanyte}
 touch nidorino3/{leafeon,azurill}
 echo -e "Способноти \nSand-Attack Razor Leaf Quick Attack Grasswhistle Magical Leaf Giga \nDrain Swords Dance Synthesis Suny Day Last Resort Leaaf \nBlade" > leafeon
 echo "weigth=4.4 height=8.0 def=4"c > azurill
+
+ls -lR
 
 echo "________________________________________________________"
 
@@ -31,16 +33,16 @@ chmod 577 ekans8
 chmod a=rwx  ekans8/claydol
 chmod 307 ekans8/grumpig
 chmod 355 ekans8/torterra
-chmod u= wx go=x gallade2
+chmod 661 gallade2
 chmod u=rw go=x gallade2/purrloin
-chmod 736 gallade2/scyther
+chmod 736 gallade2/scythe
 chmod 006 gallade2/shinx
 chmod 307 nidorino3
-chmod u= wx go=x nidorino3/shellder
+chmod u=wx go=x nidorino3/shellder
 chmod 046 nidorino3/leafeon
 chmod 404 nidorino3/azurill
 chmod 571 nidorino3/omanyte
-chmod a=r pikachu1 
+chmod a=r pikachu1
 
 ls -lR
 
@@ -48,22 +50,22 @@ echo "________________________________________________________"
 
 #3
 
-chmod -R 744 lab0/gallade2
+chmod -R 744 gallade2
 ln -s ../armaldo3 gallade2/purrloinarmaldo
 
-chmod u=rwx nidorino3/leafeon
-chmod u+wx nidorino3/azurill
+chmod 744 nidorino3/leafeon
+chmod 744 nidorino3/azurill
 cat nidorino3/leafeon nidorino3/azurill > bayleef6_63
 ln pikachu1 gallade2/purrloinpikachu
 cp bayleef6 nidorino3/azurillbayleef
 chmod 046 nidorino3/leafeon
 chmod 404 nidorino3/azurill
-chmod 736 gallade2/scyther
-chmod 006 gallade2/shinx
 
-chmod u+r ekans8/torterra
+chmod 744 ekans8/torterra
 cp -r gallade2/ ekans8/torterra
 chmod 355 ekans8/torterra
+chmod 736 gallade2/scythe
+chmod 006 gallade2/shinx
 
 chmod u+wx pikachu1
 chmod u+w nidorino3/omanyte
@@ -72,10 +74,14 @@ cp pikachu1 nidorino3/omanyte
 chmod 571 nidorino3/omanyte
 chmod a=r pikachu1 
 
+ls -lR
+
 echo "________________________________________________________"
 
 #4
+chmod 744 pikachu1
 wc -m >>pikachu1 2>/tmp/myerrors
+chmod a=r pikachu1
 ls -lFR ./lab0/* 2>/dev/null | grep ^a | grep -v[@/] | tail -3 |  sort -rs 
 cat -n ekans8 2>/tmp/myerrors| grep Mu |
 ls -lFR ./lab0/* 2>/dev/null | grep g$ | grep -v[@/] | head -3 |  sort -k6
