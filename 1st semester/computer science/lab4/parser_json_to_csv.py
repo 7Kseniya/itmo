4fd4c97549f1d06b952f5c2c02ffc5777747c902
Ksenya <114088092+7Kseniya@users.noreply.github.com>
2022-11-28 04:14:08 +0300

    Create parser_json_to_csv.py

---
+++ 1st semester/computer science/lab4/parser_json_to_csv.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+import json
+import csv
+
+data = ''
+with open('timetable.json', 'r') as f:
+    for line in f.read():
+        data += line  # заполняем список строками из файла
+data = json.loads(data)
+
+with open('timetable.csv', 'w') as f:
+    writer = csv.DictWriter(f, data[0].keys())  # ссылаемся на первый элемент в словаре и вызываем его значение
+    writer.writeheader()
+    for name in data:
+        writer.writerow(name)
