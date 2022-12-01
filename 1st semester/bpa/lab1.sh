27d41313d890ef9757e44a94eccac6f925abc4b6
Ksenya <114088092+7Kseniya@users.noreply.github.com>
2022-12-01 19:39:03 +0300

    Update lab1.sh

--- 1st semester/bpa/lab1.sh
+++ 1st semester/bpa/lab1.sh
@@ -11,8 +11,8 @@ echo -e "Ходы Body \nSlam Counter Cover Defence Curl Double-Edge Dynamicpuch
 
 mkdir -p ekans8/{claydol,grumpig,torterra}
 mkdir -p gallade2/scythe
-touch gallade2/{purrloin,shinx}
-echo -e "Способности Scratch Growl Assist \nSad-Attack Fury Swipes Pursuit Torment Fake Out Hone Claws Assurance \nSlash Captivate Night Slash Satch Nasty Plot Sucker \nPunch" > purrloin
+touch gallade2/{purloin,shinx}
+echo -e "Способности Scratch Growl Assist \nSad-Attack Fury Swipes Pursuit Torment Fake Out Hone Claws Assurance \nSlash Captivate Night Slash Satch Nasty Plot Sucker \nPunch" > purloin
 echo "Развитые способности Guts" > shinx
 
 
@@ -34,12 +34,12 @@ chmod a=rwx  ekans8/claydol
 chmod 307 ekans8/grumpig
 chmod 355 ekans8/torterra
 chmod 661 gallade2
-chmod u=rw go=x gallade2/purrloin
-chmod go=x gallade2/purrloin
+chmod u=rw gallade2/purloin
+chmod go=x gallade2/purloin
 chmod 736 gallade2/scythe
 chmod 006 gallade2/shinx
 chmod 307 nidorino3
-chmod u=wx go=x nidorino3/shellder
+chmod u=wx nidorino3/shellder
 chmod go=x nidorino3/shellder
 chmod 046 nidorino3/leafeon
 chmod 404 nidorino3/azurill
@@ -51,9 +51,9 @@ ls -lR
 echo "________________________________________________________"
 
 #3
-ln -s ../armaldo3 gallade2/purrloinarmaldo
+ln -s ../armaldo3 gallade2/purloinarmaldo
 cat nidorino3/leafeon nidorino3/azurill > bayleef6_63
-ln pikachu1 gallade2/purrloinpikachu
+ln pikachu1 gallade2/purloinpikachu
 cp bayleef6 nidorino3/azurillbayleef
 cp -r gallade2/ ekans8/torterra
 ln -s ekans8 Copy_31
@@ -76,8 +76,8 @@ echo "________________________________________________________"
 #5
 rm -f bayleef6
 rm -f gallade2/shinx
-rm -f gallade2/purrloinarmal*
-rm -f gallade2/purrloinpikac*
+rm -f gallade2/purloinarmal*
+rm -f gallade2/purloinpikac*
 rm -rf gallade2
 rm -rf ekans8/torterra
 
