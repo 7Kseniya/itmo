4188cff01a25103ea3a3be3f8e3ea6679afe745e
Ksenya <114088092+7Kseniya@users.noreply.github.com>
2023-03-01 14:52:54 +0300

    [ADD] add 1st lab sql scripts

---
+++ 2nd semester/fill_with_data.sql
@@ -0,0 +1,20 @@
+INSERT INTO SPACESHIPS VALUES
+    (1, 'Discovery One'),
+    (2, 'Apollo-13'),
+    (3, 'Endurance');
+
+INSERT INTO MODULES VALUES
+    (1, 'habitation modules'),
+    (2, 'jet engine'),
+    (3, 'facilities'),
+    (4, 'centrifuges');
+
+INSERT INTO EQUIPMENT VALUES
+    (1, 'space gondolas', 'construction works in space', 3, 1),
+    (2, 'computers', 'fight controls', 3, 1),
+    (3, 'medical department', 'provide medical care', 1, 1);
+
+INSERT INTO TITLES VALUES
+    (1, 'Anna', 1),
+    (2, 'Betty', 1),
+    (3, 'Clara', 1);
